a23ab8b9f8908550aac3ca128dba3ab67af96641
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-13 00:01:00 +0300

    add build component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return (
+        <div className="">
+            <header>
+                <Link to="/" className="logo">MASLZA ENGINEERS</Link>
+                <nav className="header-navlinks-parent">
+                    <NavLink
+                        to="about"
+                        className='header-navlinks'
+                    >About</NavLink>
+                    <NavLink
+                        to="vendor"
+                        className='header-navlinks'
+                    >Vendor</NavLink>
+                    <NavLink
+                        to="equipment"
+                        className='header-navlinks'
+                    >Equipment</NavLink>
+                   
+                </nav>
+            </header>
+            <div className="outlet--container">
+                <Outlet />
+            </div>
+            <footer>
+
+            </footer>
+        </div>
+    )
+}
