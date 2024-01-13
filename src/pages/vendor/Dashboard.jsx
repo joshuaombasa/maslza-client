5f539d29b2f35e75a7ec55539fe2f006c1f5148d
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-13 23:58:28 +0300

    add buid page file

---
+++ src/pages/vendor/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Dashboard() {
+
+   
+
+    return (
+        <div className="dashboard-page">
+            <h1>This is dashboard page</h1>
+        </div>
+    )
+}
