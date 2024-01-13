2d6aeb9e2848b919a4001742cfffcdaf75b5b9f5
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-13 23:57:02 +0300

    added root routing file

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
+import VendorLayout from './components/VendorLayout'
 import About from './pages/About'
 import Equipment from './pages/Equipment'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import Dashboard from './pages/vendor/Dashboard'
+import Income from './pages/vendor/Income'
+import Reviews from './pages/vendor/Reviews'
+import VendorEquipment from './pages/vendor/VendorEquipment'
 
 function App() {
  const router = createBrowserRouter(createRoutesFromElements(
@@ -14,6 +19,12 @@ function App() {
       <Route index element={<Home/>}/>
       <Route path='about' element={<About/>}/>
       <Route path='equipment'  element={<Equipment/>}/>
+      <Route path='vendor'  element={<VendorLayout/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='income' element={<Income/>}/>
+        <Route path='equipment' element={<VendorEquipment/>}/>
+        <Route path='reviews' element={<Reviews/>}/>
+      </Route>
       <Route path='*' element={<NotFound/>}/>
     </Route>
 
