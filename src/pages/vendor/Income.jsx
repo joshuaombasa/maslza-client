b657c3826a7f29edfea7cf6b6701ccb23bac60d1
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-13 23:58:19 +0300

    add buid page file

---
+++ src/pages/vendor/Income.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Income () {
+    return (
+        <div className="income-page">
+            <h1>This is income page</h1>
+        </div>
+    )
+}
