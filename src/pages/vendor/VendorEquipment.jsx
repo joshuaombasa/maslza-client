43219f98b3c6e180377f1cf8c6d0987c77451a3d
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-13 23:57:55 +0300

    add buid page file

---
+++ src/pages/vendor/VendorEquipment.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function VendorEquipment() {
+    return (
+        <div className="vendorEquipment-page">
+            <h1>This is Vendor Equipment page</h1>
+        </div>
+    )
+}
