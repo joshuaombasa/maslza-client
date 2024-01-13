a8f9aa72bcb4c578b5bfcb5648717e1a1b29ca70
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-13 23:57:34 +0300

    add buid page file

--- src/pages/Equipment.jsx
+++ src/pages/Equipment.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { useSearchParams } from 'react-router-dom'
 
 export default function Equipment() {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    function generateNewUrlSearchParams(type, value) {
+        const sp = new URLSearchParams()
+        sp.set(type, value)
+        setSearchParams(sp)
+    }
+
     return (
         <div className="">
             <h1>Listed  equipment</h1>
+            <nav className="vendor-layout-nav">
+                <button
+                    onClick={() => generateNewUrlSearchParams("type", "truck")}
+                >Truck</button>
+                <button
+                    onClick={() => generateNewUrlSearchParams("type", "grader")}
+                >Grader</button>
+                <button
+                    onClick={() => generateNewUrlSearchParams("type", "crane")}
+                >Crane</button>
+                <button
+                    onClick={() => generateNewUrlSearchParams("type", "loader")}
+                >Loader</button>
+                <button
+                    onClick={() => generateNewUrlSearchParams("type", "excavator")}
+                >Excavator</button>
+            </nav>
         </div>
     )
 }
