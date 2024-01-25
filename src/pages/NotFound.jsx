3571d9acc088237c4c7d5df2df3699405646956f
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-26 01:04:02 +0300

    updated build page logic

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFound () {
     return (
         <div className="">
             <h1>Page not found</h1>
+            <Link to="/">Return to homepage</Link>
         </div>
     )
-}
+} 
