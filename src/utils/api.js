b12c8353ae20873139e5271ba4c77934cfd20b43
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-30 22:04:33 +0300

    update login function logic

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,35 +1,77 @@
 
 
 async function getEquipment() {
+
     try {
         const res = await fetch(`http://localhost:4001/api/equipment`)
+
         const data = await res.json()
+
         if (!res.ok) {
             throw {
                 message: data.message,
-                status: res.status
+                status: res.status,
+                statusText: res.statusText,
             }
         }
         return data
+
     } catch (error) {
+
         return error
+
     }
 }
 
 async function getSelectedEquipment(id) {
     try {
         const res = await fetch(`http://localhost:4001/api/equipment/${id}`)
+
         const data = await res.json()
+
         if (!res.ok) {
             throw {
                 message: data.message,
-                status: res.status
+                status: res.status,
+                statusText: res.statusText,
             }
         }
+
         return data
+
+    } catch (error) {
+
+        return error
+
+    }
+}
+
+async function loginUser(userData) {
+    console.log(userData)
+    try {
+        const res = await fetch("http://localhost:4001/api/login", {
+            method: "post",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+
+        const data = await res.json()
+
+        if (!res.ok) {
+            throw {
+                message: data.message,
+                status: res.status,
+                statusText: res.statusText,
+            }
+        }
+
+        return data
+
     } catch (error) {
         return error
     }
 }
 
-export { getEquipment, getSelectedEquipment }
+export { getEquipment, getSelectedEquipment, loginUser }
