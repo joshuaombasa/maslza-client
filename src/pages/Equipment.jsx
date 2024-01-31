b670c4f092bfacdbc1bd737c35511230ca0cdca8
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-31 23:15:52 +0300

    updated build page

--- src/pages/Equipment.jsx
+++ src/pages/Equipment.jsx
@@ -3,7 +3,7 @@ import { Await, defer, useLoaderData, useSearchParams, Link } from 'react-router
 import { getEquipment } from "../utils/api";
 
 export async function loader() {
-    return defer({equipment : getEquipment()})
+    return defer({ equipment: getEquipment() })
 }
 
 export default function Equipment() {
@@ -14,17 +14,27 @@ export default function Equipment() {
 
     function generateNewUrlSearchParams(type, value) {
         const sp = new URLSearchParams()
-        sp.set(type, value)
+        if (!value) {
+            sp.delete(type)
+        } else {
+            sp.set(type,value)
+        }
         setSearchParams(sp)
     }
 
+    const equipmentType = searchParams.get('type')
+  
+
     function renderEquipment(equipment) {
-        
-        const equipmentEls = equipment.map(item => (
+
+        const filteredEquipment = equipmentType  ? equipment.filter(item => item.type === equipmentType) : equipment
+
+
+        const equipmentEls = filteredEquipment.map(item => (
             <Link to={item._id} key={item._id} className="equipment-item">
-                <img src={item.imageUrl}/>
+                <img src={item.imageUrl} />
                 <h3>{item.name}</h3>
-                <h3>{item.price}</h3>
+                <h3>$ {item.price}</h3>
             </Link>
         ))
 
@@ -32,30 +42,35 @@ export default function Equipment() {
     }
 
     return (
-        <div className="">
+        <div className="equipment-page">
             <h1>Listed  equipment</h1>
             <nav className="vendor-layout-nav">
                 <button
-                    onClick={() => generateNewUrlSearchParams("type", "truck")}
+                    onClick={() => generateNewUrlSearchParams("type", "Truck")}
                 >Truck</button>
                 <button
-                    onClick={() => generateNewUrlSearchParams("type", "grader")}
-                >Grader</button>
+                    onClick={() => generateNewUrlSearchParams("type", "Van")}
+                >Van</button>
                 <button
-                    onClick={() => generateNewUrlSearchParams("type", "crane")}
+                    onClick={() => generateNewUrlSearchParams("type", "Crane")}
                 >Crane</button>
                 <button
-                    onClick={() => generateNewUrlSearchParams("type", "loader")}
+                    onClick={() => generateNewUrlSearchParams("type", "Loader")}
                 >Loader</button>
                 <button
-                    onClick={() => generateNewUrlSearchParams("type", "excavator")}
+                    onClick={() => generateNewUrlSearchParams("type", "Excavator")}
                 >Excavator</button>
+                <button
+                    onClick={() => generateNewUrlSearchParams("type", null)}
+                >Clear filters</button>
             </nav>
-            <Suspense  fallback={<h1>Loading...</h1>}>
-                <Await resolve={dataPromise.equipment}>
-                    {renderEquipment}
-                </Await>
-            </Suspense>
+            <div className="equipment-container">
+                <Suspense fallback={<h1>Loading...</h1>}>
+                    <Await resolve={dataPromise.equipment}>
+                        {renderEquipment}
+                    </Await>
+                </Suspense>
+            </div>
         </div>
     )
 }
